4e2ea60bfc8bd92b582d485460db33067b3ffe4f
jonasnascimento <jonas.soares.nascimento@gmail.com>
2024-04-15 14:16:40 -0300

    R$

--- scripts.js
+++ scripts.js
@@ -18,7 +18,7 @@ function showAll(productsArray) {
     <li>
     <img src=${product.src}>
     <p>${product.name}</p>
-    <p class="item-price">R$ ${formatCurrency(product.price)}</p>
+    <p class="item-price"> ${formatCurrency(product.price)}</p>
     </li>
 `
     })
